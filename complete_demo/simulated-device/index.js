a43b46cfb38ae45a6c8e45a186efae38c09de9a8
Wechuli <wechulipaul@yahoo.com>
2019-10-22 11:19:25 +0300

    fixing bugs

--- complete_demo/simulated-device/index.js
+++ complete_demo/simulated-device/index.js
@@ -1,69 +1,64 @@
-
 "use strict";
 
-module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();    
-    // Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+module.exports = function(context, myTimer) {
+  var timeStamp = new Date().toISOString();
+  // Copyright (c) Microsoft. All rights reserved.
+  // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+  // The device connection string to authenticate the device with your IoT hub.
+  //
+  // NOTE:
+  // For simplicity, this sample sets the connection string in code.
+  // In a production environment, the recommended approach is to use
+  // an environment variable to make it available to your application
+  // or use an HSM or an x509 certificate.
+  // https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
+  //
+  // Using the Azure CLI:
+  // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+  var connectionString =
+    "HostName=wechulihub.azure-devices.net;DeviceId=mySimulatedDevice;SharedAccessKey=Q/lAQre6dAHc+LwmT5oIhMQxAhj28bRurjuq0+ak+0o=";
 
+  // Using the Node.js Device SDK for IoT Hub:
+  //   https://github.com/Azure/azure-iot-sdk-node
+  // The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+  var Mqtt = require("azure-iot-device-mqtt").Mqtt;
+  var DeviceClient = require("azure-iot-device").Client;
+  var Message = require("azure-iot-device").Message;
 
-// The device connection string to authenticate the device with your IoT hub.
-//
-// NOTE:
-// For simplicity, this sample sets the connection string in code.
-// In a production environment, the recommended approach is to use
-// an environment variable to make it available to your application
-// or use an HSM or an x509 certificate.
-// https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
-//
-// Using the Azure CLI:
-// az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
-var connectionString =
-  "HostName=iotcentralhub.azure-devices.net;DeviceId=temp_humidity_sensor;SharedAccessKey=9egCeUCtnb6+P/4Xv3pgd09BhhfaBws8heLnNho8qbA=";
+  var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+  let deviceId = "temp_humidity_sensor";
 
-// Using the Node.js Device SDK for IoT Hub:
-//   https://github.com/Azure/azure-iot-sdk-node
-// The sample connects to a device-specific MQTT endpoint on your IoT Hub.
-var Mqtt = require("azure-iot-device-mqtt").Mqtt;
-var DeviceClient = require("azure-iot-device").Client;
-var Message = require("azure-iot-device").Message;
+  // Create a message and send it to the IoT hub every second
+  function sendMessage() {
+    // Simulate telemetry.
+    var temperature = 20 + Math.random() * 15;
+    var message = new Message(
+      JSON.stringify({
+        temperature: parseFloat(temperature.toFixed(2)),
+        humidity: parseFloat((60 + Math.random() * 30).toFixed(2)),
+        deviceId
+      })
+    );
 
-var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
-let deviceId = "temp_humidity_sensor";
+    // Add a custom application property to the message.
+    // An IoT hub can filter on these properties without access to the message body.
+    message.properties.add(
+      "temperatureAlert",
+      temperature > 30 ? "true" : "false"
+    );
 
-// Create a message and send it to the IoT hub every second
-function sendMessage() {
-  // Simulate telemetry.
-  var temperature = 20 + Math.random() * 15;
-  var message = new Message(
-    JSON.stringify({
-      
-      temperature: parseFloat(temperature.toFixed(2)),
-      humidity: parseFloat((60 + Math.random() * 30).toFixed(2)),
-      deviceId
-    })
-  );
+    console.log("Sending message: " + message.getData());
 
-  // Add a custom application property to the message.
-  // An IoT hub can filter on these properties without access to the message body.
-  message.properties.add(
-    "temperatureAlert",
-    temperature > 30 ? "true" : "false"
-  );
+    // Send the message.
+    client.sendEvent(message, function(err) {
+      if (err) {
+        console.error("send error: " + err.toString());
+      } else {
+        console.log("message sent");
+      }
+    });
+  }
 
-  console.log("Sending message: " + message.getData());
-
-  // Send the message.
-  client.sendEvent(message, function(err) {
-    if (err) {
-      console.error("send error: " + err.toString());
-    } else {
-      console.log("message sent");
-    }
-  });
-};
-
-sendMessage();
-
+  sendMessage();
 };
